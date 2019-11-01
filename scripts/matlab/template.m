25344420375efaf82415b99a1533744799e3a4ec
jkwasnio <57213062+jkwasnio@users.noreply.github.com>
2019-11-01 16:56:07 +0100

    extended comments in matlab script template

--- scripts/matlab/template.m
+++ scripts/matlab/template.m
@@ -1,5 +1,8 @@
-% standart prelude
+%%% standart prelude %%%
+% change path to current working directory
+% (matlab does NOT do this automatically!)
 cd(getenv('PWD'));
+% load parameters
 load parameters;
 
 % example
