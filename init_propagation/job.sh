ababd0f90d87db94b871c09cdb27b77b0821eaa8
jkwasnio <57213062+jkwasnio@users.noreply.github.com>
2019-11-06 10:11:03 +0100

    added automatic transfer of wave function file in propagation job script

--- init_propagation/job.sh
+++ init_propagation/job.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-
+# transfer initial wave function from relaxation
+cp ../relaxation/restart ./initial_wavefunction.dat
+# run propagation
 {ML_X_PATH}/Bin/qdtk_propagate.x -rst initial_wavefunction.dat -opr hamiltonian.dat -dt {dt} -tfinal {tfinal} -itg dp5 -atol 1e-12 -psi -rstzero
